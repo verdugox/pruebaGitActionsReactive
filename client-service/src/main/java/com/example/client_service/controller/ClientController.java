c28a88adc3c1afec697537c7efa0b7467a59a3a5
verdugox <luisandresverdugox@gmail.com>
2024-12-17 17:33:26 -0500

    feat: corrections endpoints controller

--- client-service/src/main/java/com/example/client_service/controller/ClientController.java
+++ client-service/src/main/java/com/example/client_service/controller/ClientController.java
@@ -2,6 +2,7 @@ package com.example.client_service.controller;
 
 import com.example.client_service.model.Client;
 import com.example.client_service.service.ClientService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,21 +16,26 @@ public class ClientController {
     public ClientController(ClientService service) {
         this.service = service;
     }
+
     @GetMapping
     public Flux<Client> getAllClients() {
-           return service.getAllClients();
-    }
-    @GetMapping("/{id}")
-    public Mono<Client> getClientById(@PathVariable String id) {
-           return service.getClientById(id);
-    }
-    @PostMapping
-    public Mono<Client> createClient(@RequestBody Client client){
-        return service.saveClient(client);
-    }
-    @DeleteMapping
-    public Mono<Void> deleteClient(@PathVariable String id){
-        return service.deleteClient(id);
+        return service.getAllClients();
     }
 
+    @GetMapping("/{id}")
+    public Mono<Client> getClientById(@PathVariable String id) {
+        return service.getClientById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Client> createClient(@RequestBody Client client) {
+        return service.saveClient(client);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteClient(@PathVariable String id) {
+        return service.deleteClient(id);
+    }
 }
