03e9db84ac43e8107ec3e4cbc0d58fb9209db12a
verdugox <luisandresverdugox@gmail.com>
2025-02-21 13:45:20 -0500

    feat: Subiendo nuevo sistema SORTEC v2 - JWToken

--- client-service/src/main/java/com/example/client_service/security/SecurityConfig.java
+++ client-service/src/main/java/com/example/client_service/security/SecurityConfig.java
@@ -28,7 +28,14 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable) // 🔹 Deshabilitar CSRF
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // 🔹 Se usa configuración separada
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/api/auth/login", "/api/clients" , "/api/sorteos" , "/api/ganadores").permitAll() // 🔹 Permitir el login sin autenticación
+                        .pathMatchers(
+                                "/api/auth/login",
+                                "/api/clients",
+                                "/api/sorteos",
+                                "/api/ganadores",
+                                "/api/approve/{id}",          // 🔹 Permitir aprobación de clientes sin autenticación
+                                "/api/approve-payment"       // 🔹 Permitir aprobación de pagos sin autenticación
+                        ).permitAll() // 🔹 Permitir acceso sin autenticación
                         .pathMatchers("/api/menu").authenticated()
                         .pathMatchers("/api/clients/admin/**").hasRole("ADMINISTRADOR")
                         .pathMatchers("/api/clients/perfil").hasAnyRole("ADMINISTRADOR", "PARTICIPANTE")
@@ -38,6 +45,7 @@ public class SecurityConfig {
                 .addFilterAt(new JwtAuthenticationFilter(jwtUtil), SecurityWebFiltersOrder.AUTHENTICATION) // 🔹 Agregar filtro JWT
                 .build();
     }
+
 
     @Bean
     public CorsWebFilter corsWebFilter() {
